ce9a8a3b9da42d362242bfb2d078433d5750a27b
Sarthak Satpathy <ssatpa5@pmedseq.bmi.emory.edu>
2021-09-21 17:17:31 -0400

    main update scaling PCA

--- pack/main.py
+++ pack/main.py
@@ -14,4 +14,18 @@ data = data.drop('ID', axis=1) # removing the ID column and keeping gene express
 X = data # X stores the matrix of gene expression data 
 y = labels["Class"] # y stores the labels associated with that (PRAD, BRCA, KIRC, LUAD, COAD)
 
+# scaling the datasets by columns to avoid 
+scalefn = preprocessing.StandardScaler()
+scalefn.fit(X)
+X_scaled_array = scalefn.transform(X) # the values are transformed to a scaled value
+X_scaled = pd.DataFrame(X_scaled_array, columns = X.columns)  
+
+# Dimension Reduction
+ndimensions = 5 # enter the number of Principle components that you want
+seed = 0 # seed set to 0 
+pca = PCA(n_components=ndimensions, random_state=seed)
+pca.fit(X_scaled)
+X_pca_array = pca.transform(X_scaled) # this array stores the 
+#X_pca = pd.DataFrame(X_pca_array, columns=['PC1','PC2']) 
+X_pca = pd.DataFrame(X_pca_array, columns=['PC1','PC2','PC3','PC4','PC5']) # Add the column names according to the ndimensions
 
